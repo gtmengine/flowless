8e9e6641e2c71f56cb3fa0ebb24bd56f34c68e28
Georgy <nizhge@gmail.com>
2025-10-20 06:44:30 +0300

    Restore original About text with simplified Support section

--- news-feed-eradicator-master/src/components/info-panel.ts
+++ news-feed-eradicator-master/src/components/info-panel.ts
@@ -19,22 +19,22 @@ const About = () => {
 			h('h2', 'About'),
 			h(
 				'p',
-				'Flowless helps you take back control from addictive social media feeds. ' +
-					'Many people find themselves unconsciously typing social media URLs into their browser, ' +
-					'losing hours to endless scrolling without even realizing it.'
+				'Flowless was born in 2012 in a bout of procrastination. I first noticed that ' +
+					"I wasn't in control of my own mind when I found myself typing " +
+					'"facebook.com" into the address bar unconsciously.'
 			),
 			h(
 				'p',
-				'Social media platforms have perfected the art of capturing attention through their news feeds. ' +
-					'While these platforms can be useful for communication, the endless feed often becomes a time sink and source of distraction.'
+				'Though I was forced to use Facebook for my studies, I realised I just had to find a way to reduce its addictive power over me. ' +
+					'The number one thing that felt so addictive about it was the news feed.'
 			),
 			h(
 				'p',
-				'Flowless blocks these addictive feeds and replaces them with inspiring quotes, cat pictures, or a blank space - ' +
-					"giving you back control of your time and attention. It's your browser, your rules."
+				"In recent years, we've seen the news feed proliferate across all sort of apps thanks to " +
+					"its addictive power over users. I think it's long overdue that we as users take that power back."
 			),
 			h('p', [
-				'Flowless is completely free, ',
+				'Flowless is and always will be free and ',
 				h(
 					'a.underline-hover',
 					{
@@ -44,34 +44,29 @@ const About = () => {
 					},
 					'open-source'
 				),
-				', and will never track you or collect your data.',
-			]),
-		]),
-		h('div.v-stack-2', [
-			h('h2', 'Features'),
-			h('ul', [
-				h('li', 'Block feeds on Facebook, Twitter/X, Instagram, LinkedIn, Reddit, YouTube, GitHub, and Hacker News'),
-				h('li', 'Replace feeds with inspiring quotes or cute cat pictures'),
-				h('li', 'Custom word replacement to make social media content more interesting'),
-				h('li', 'Temporary "Show feed" option when you need it'),
-				h('li', 'Dark mode interface'),
-				h('li', 'Complete privacy - no tracking, no data collection'),
+				', and will never track you.',
 			]),
 		]),
 		h('div.v-stack-2', [
 			h('h2', 'Support'),
-			h('p', [
-				'View source code, report bugs, or contribute on ',
-				h(
-					'a.underline-hover',
-					{
-						props: {
-							href: 'https://github.com/gtmengine/Flowless-',
+			h(
+				'p',
+				"If Flowless has saved you time or mental energy and you'd like to help out, here's how you can do so:"
+			),
+			h('ul', [
+				h('li', 'Tell your friends about it'),
+				h('li', [
+					'Report or fix bugs via ',
+					h(
+						'a.underline-hover',
+						{
+							props: {
+								href: 'https://github.com/gtmengine/Flowless-',
+							},
 						},
-					},
-					'GitHub'
-				),
-				'.',
+						'GitHub'
+					),
+				]),
 			]),
 		]),
 	]);
